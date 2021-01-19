22f0f2f0ff496289c390c0e56a594da414f95f9b
Nitzan Listman <nitzanjl@gmail.com>
2021-01-19 20:13:44 +0200

    added tests + template

---
+++ Exercise.test.js
@@ -0,0 +1,80 @@
+const { expect } = require("@jest/globals");
+const {
+  myReverse,
+  allCombinations,
+  allCaps,
+  myPower,
+  getFirstNotRepeating,
+  isPrefectNumber,
+} = require("./Exercises");
+
+describe("Question 1", () => {
+  it("Should reverse a string", () => {
+    const input = "Nitzan";
+    const expOutput = "naztiN";
+    const output = myReverse(input);
+
+    expect(expOutput).toBe(output);
+  });
+});
+
+describe("Question 2", () => {
+  it("Should get all combinations of a string", () => {
+    const input = "dog";
+    const expOutput = ["d", "do", "dog", "o", "og", "og", "g"];
+    const output = allCombinations(input);
+
+    expect(expOutput).toEqual(output);
+  });
+});
+
+describe("Question 3", () => {
+  it("Should capitalize all words in sentance", () => {
+    const input = "i am a cyber4s student";
+    const expOutput = "I Am A Cyber4s Student";
+    const output = allCaps(input);
+
+    expect(expOutput).toBe(output);
+  });
+});
+
+describe("Question 4", () => {
+  it("Should perform a mathematic power method", () => {
+    const input = [
+      Math.floor(Math.random(10) + 1),
+      Math.floor(Math.random(5) + 1),
+    ];
+    const expOutput = Math.pow(input[0], input[1]);
+    const output = myPower(input[0], input[1]);
+
+    expect(expOutput).toBe(output);
+  });
+});
+
+describe("Question 5", () => {
+  it("Should get the first non repeating character in a string", () => {
+    const input = "abanibiabohebe";
+    const expOutput = "n";
+    const output = getFirstNotRepeating(input);
+
+    expect(expOutput).toBe(output);
+  });
+});
+
+describe("Question 6", () => {
+  it("Should check if a number is a perfect number (optional)", () => {
+    const giveUp = "I'm not interested in the bonus question :(";
+    const perfectNumber = 496;
+    const regularNumber = 7;
+    const outputTrue = isPrefectNumber(perfectNumber);
+    const outputFalse = isPrefectNumber(regularNumber);
+
+    if (outputTrue === giveUp && outputFalse === giveUp) {
+      expect(true).toBe(true);
+    } else {
+      console.log(outputTrue);
+      expect(outputTrue).toBe(true);
+      expect(outputFalse).toBe(false);
+    }
+  });
+});
---
+++ Exercises.js
@@ -0,0 +1,51 @@
+//Question 1
+function myReverse(str) {
+  // your code here
+  return "";
+}
+
+//Question 2
+function allCombinations(str) {
+  let combinations = [];
+  // your code here
+
+  //
+  return combinations;
+}
+
+//Question 3
+function allCaps(str) {
+  // your code here
+  return "";
+}
+
+//Question 4
+function myPower(x, n) {
+  // your code here
+  return "";
+}
+
+//Question 5
+function getFirstNotRepeating(str) {
+  // your code here
+  return "";
+}
+
+//Question 6 (Bonus)
+function isPrefectNumber(num) {
+  // your code here
+  return "I'm not interested in the bonus question :(";
+}
+
+// *** Playground ***
+// Feel free to run and test your code here on your own
+
+// *** End of Playground ***
+
+// Don't touch me :)
+exports.myReverse = myReverse;
+exports.allCombinations = allCombinations;
+exports.allCaps = allCaps;
+exports.myPower = myPower;
+exports.getFirstNotRepeating = getFirstNotRepeating;
+exports.isPrefectNumber = isPrefectNumber;
